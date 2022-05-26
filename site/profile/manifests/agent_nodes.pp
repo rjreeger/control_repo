41d26e3c70ff37bae0f3dce1b263c00b7e8fd426
rjreeger <105824342+rjreeger@users.noreply.github.com>
2022-05-26 14:29:23 -0600

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
-  include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  include samuelson-dockeragent
+  samuelson-dockeragent::node {'web.puppet.vm':}
+  samuelson-dockeragent::node {'db.puppet.vm':}
 }
